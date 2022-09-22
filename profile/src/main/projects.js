2963794abf99831d5813e721f59bbe8679c9f7a5
weirdo <a@b.c>
2022-09-22 14:23:04 +1000

    changed project descriptions and refactored code

--- profile/src/main/projects.js
+++ profile/src/main/projects.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import ProjectTile from './project-tile';
 
-export const Autotrader = () => (
-    <ProjectTile
-        title={'project: algo trader'}
-        project_image_link={'https://images.squarespace-cdn.com/content/v1/5a459ea9692ebe7dcf9ca9ff/1515028331771-FGZ2K9P0VRAB1Q80OYSR/image-asset.jpeg?format=1500w'}
-        description={'algorithmic trader written in python and hosted on AWS'}
-        project_link={'/trader'}
-        github_link={'https://github.com/usr-tony/autotrade'}
-    />
-)
+export const Autotrader = () => ProjectTile({
+    title: 'Project: Trader',
+    project_image_link: 'https://images.squarespace-cdn.com/content/v1/5a459ea9692ebe7dcf9ca9ff/1515028331771-FGZ2K9P0VRAB1Q80OYSR/image-asset.jpeg?format=1500w',
+    description: 'algorithmic trader written in python and hosted on AWS',
+    project_link: '/trader',
+    github_link: 'https://github.com/usr-tony/autotrade',
+})
 
-export function Jobs () {
-    let props = {
-        title: 'project: job application bot',
-        project_image_link: 'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744',
-        description: `
-            applies for jobs automatically on seek.com.au,
-            also scrapes and organises jobs into data to describe the 
-            current jobs in demand in the market right now
-        `,
-        project_link: '/jobs',
-        github_link: 'https://github.com/usr-tony/job-search',
-    }
-    return (
-        <ProjectTile {...props} />
-    )
-}
+export const Jobs = () => ProjectTile({
+    title: 'Project: Jobs',
+    project_image_link: 'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744',
+    description: (
+        <ul style={{position: 'relative', left: '-1rem'}}>
+            <li>Scrapes jobs automatically on seek.com.au</li>
+            <li>Visualises and filters jobs based on various criteria</li>
+            <li>Hosted on AWS</li>
+        </ul>
+    ),
+    project_link: '/jobs',
+    github_link: 'https://github.com/usr-tony/job-search',
+})
--- profile/src/trader/breadcrumbs.js
+++ profile/src/trader/breadcrumbs.js
@@ -3,35 +3,33 @@ import { Link, Breadcrumbs, Typography, Paper } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home'
 import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin'
 
-export default (props) => {
-    return (
-        <Paper className='container-fluid' sx={{ p: 1, m: 0.5, width: 'fit-content', height: 34 }} elevation={3}>
-            <Breadcrumbs separator="/" aria-label="breadcrumb" sx={{ fontSize: 12 }}>
-                <Link 
-                    underline="hover"  
-                    key="1" 
-                    color='inherit'
-                    href="/" 
-                    sx={{ 
-                        display: 'flex', 
-                        alignItems: 'center', 
-                        mr: 0.5, 
-                        fontSize: 'inherit', 
-                        '&:hover': { color: 'inherit' 
-                    }}}
-                >
-                    <HomeIcon sx={{ mr: 0.7, fontSize: 15 }}/> 
-                    profile 
-                </Link>
-                <Typography 
-                    key="3" 
-                    color="text.primary" 
-                    sx={{ display: 'flex', alignItems: 'center', fontSize: 'inherit' }}
-                >
-                    <CurrencyBitcoinIcon sx={{ mr: 0.5, fontSize: 15 }} /> 
-                    trader
-                </Typography>
-            </Breadcrumbs>
-        </Paper>
-    )
-}
+export default (props) => (
+    <Paper className='container-fluid' sx={{ p: 1, m: 0.5, width: 'fit-content', height: 34 }} elevation={3}>
+        <Breadcrumbs separator="/" aria-label="breadcrumb" sx={{ fontSize: 12 }}>
+            <Link 
+                underline="hover"  
+                key="1" 
+                color='inherit'
+                href="/" 
+                sx={{ 
+                    display: 'flex', 
+                    alignItems: 'center', 
+                    mr: 0.5, 
+                    fontSize: 'inherit', 
+                    '&:hover': { color: 'inherit' }
+                }}
+            >
+                <HomeIcon sx={{ mr: 0.7, fontSize: 15 }}/> 
+                profile 
+            </Link>
+            <Typography 
+                key="3" 
+                color="text.primary" 
+                sx={{ display: 'flex', alignItems: 'center', fontSize: 'inherit' }}
+            >
+                <CurrencyBitcoinIcon sx={{ mr: 0.5, fontSize: 15 }} /> 
+                trader
+            </Typography>
+        </Breadcrumbs>
+    </Paper>
+)
