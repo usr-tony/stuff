c7b357319bf5933300036a5d634302343350074a
tony <8inchtteemo@mgmail.com>
2024-09-28 21:14:53 +1000

    updates

--- jobs/start.py
+++ jobs/start.py
@@ -1,4 +1,3 @@
-#! /Users/tony/miniconda3/envs/jobs/bin/python
 from seek.scrape import scrape
 from time import sleep
 from datetime import datetime, timedelta
@@ -6,12 +5,13 @@ import gzip
 import shutil
 import boto3
 import os
+from pathlib import Path
 
 
 SLEEP_SECONDS = timedelta(hours=6).seconds
 AWS_BUCKET_NAME = 'jobs--001'
 SEEK_DB_PATH = './seek/jobs.db'
-UPLOAD_PERIOD: timedelta = timedelta(days=1)
+UPLOAD_PERIOD: timedelta = timedelta(days=3)
 
 
 def main():
@@ -27,28 +27,32 @@ def main():
 
 
 def upload_s3(file_name=SEEK_DB_PATH):
-    compress_file(file_name)
-    compressed_file = get_compressed_path(file_name)
-    ptime(f'uploading {compressed_file} to s3')
-    boto3.client('s3').upload_file(compressed_file, AWS_BUCKET_NAME, compressed_file)
-    os.remove(compressed_file)
+    compressed_file_path = compress_file(file_name)
+    ptime(f'uploading {compressed_file_path} to s3')
+    boto3.client('s3').upload_file(
+        compressed_file_path, 
+        AWS_BUCKET_NAME, 
+        Path(compressed_file_path).parts[-1],
+        ExtraArgs=dict(
+            StorageClass='STANDARD' # infrequent access tier
+        )
+    )
+    os.remove(compressed_file_path)
     ptime('uploaded to s3')
 
 
 def compress_file(file_name=SEEK_DB_PATH):
-    compressed_file_path = get_compressed_path(file_name)
+    compressed_file_path = f'{file_name}.gz'
     ptime(f'creating compressed file ->', compressed_file_path)
     with gzip.open(compressed_file_path, 'wb') as fout:
         with open(file_name, 'rb') as fin:
             shutil.copyfileobj(fin, fout)
 
+    return compressed_file_path
+
 
 def ptime(*msgs: str) -> None:
     print(datetime.now().isoformat(), *msgs)
-
-
-def get_compressed_path(file_name: str=SEEK_DB_PATH) -> str:
-    return f'{file_name}.gz'
 
 
 if __name__ == '__main__':
